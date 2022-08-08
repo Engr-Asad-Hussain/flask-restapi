b59e8d70f351270129ee40e7a67c7c3ce1a09b35
Asad Hussain <asad.h1998@yahoo.com>
2022-08-08 18:35:55 +0500

    added details for host='0.0.0.0

--- run.py
+++ run.py
@@ -6,4 +6,14 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
+
+# 0.0.0.0
+# When you do not specify SERVER_NAME, it takes default None value. 
+# It means that you specify app.run(host=None) command. 
+# Now, what happens when you do like this? The answer is, when you create a container 
+# from Dockerfile, and do a quick Postman hit at health_check, you will observed that 
+# the hits are not getting into your flask application even specifying the -p flag in 
+# the docker run command.
+# On the other hand, if you specify SERVER_NAME='0.0.0.0' you will observed that your 
+# docker container will now receiving hits from the outside.
