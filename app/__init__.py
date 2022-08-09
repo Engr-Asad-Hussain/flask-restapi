1053ef6b53d4009b0e2618cec3cd7051f0c5c54b
Asad Hussain <asad.h1998@yahoo.com>
2022-08-09 07:50:21 +0500

    added blueprints import in for loop.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from importlib import import_module
+
+def register_blueprints(app):
+    for moduel_name in ['user_auth']:#, 'user_profile', 'user_roles']:
+        module = import_module(f'app.{moduel_name}.views')
+        app.register_blueprint(module.bp)
 
 def create_app():
     app = Flask()
+    register_blueprints(app)
 
     return app
