d5fee4bac8b389b92eff8ee13cf2cebf165d30fc
Asad Hussain <asad.h1998@yahoo.com>
2022-08-09 07:41:42 +0500

    created response class to return the error message

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,6 @@
 from flask import make_response, jsonify
 
-def send_response(message: str, status_code: int, data=None):
+def send_response(message: str, status_code: int, data: dict = None):
     """ Creates a formatted response """
     body = { 'message': message, 'status_code': status_code }
     if data is not None:
@@ -8,3 +8,14 @@ def send_response(message: str, status_code: int, data=None):
     response = make_response(jsonify(body))
     response.status_code = status_code
     return response
+
+class Response:
+    _errors = {
+        'Exception': (send_response, 500)
+    }
+
+    def send(self, error):
+        error_type = type(error).__name__
+        error_details = str(error)
+        resp, error_code = self._errors.get(error_type, 'Exception')
+        return resp(error_details, error_code)
