868407966441b04b5bac8c61518aeb84a84d7e25
Asad Hussain <asad.h1998@yahoo.com>
2022-08-09 12:34:49 +0500

    added before/after request db sessions

--- app/database.py
+++ app/database.py
@@ -10,6 +10,7 @@ class Database:
     _conn      = None
     
     def get_conn(cls):
+        print('Database get_conn')
         try:
             conn = mariadb.connect(
                 database = cls._database,
@@ -21,3 +22,21 @@ class Database:
             cls._conn = conn
         except mariadb.Error as e:
             raise ConnectionError(f'Unable to connect database. {e}')
+    
+    def close(cls):
+        print('Database close')
+        cls._conn.close()
+        cls._conn = None
+
+
+@app.before_request
+def database_conn():
+    try:
+        Database.get_conn()
+    except Exception as e:
+        from app.utils import Response
+        return Response.send(e)
+
+@app.after_request
+def database_close():
+    Database.close()
